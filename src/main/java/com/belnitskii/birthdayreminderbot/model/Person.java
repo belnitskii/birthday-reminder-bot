a909502aff6954094a80a0f0c88d117b66f5f150
belnitskii <belnitskii@mail.ru>
2025-04-01 15:29:01 +0200

    Added constraints on name and birthdayDate fields in Person entity
    — These annotations affect the table structure in the database, not the validation in Java code.
    — The corresponding columns in the table will now have the `NOT NULL` constraint.

--- src/main/java/com/belnitskii/birthdayreminderbot/model/Person.java
+++ src/main/java/com/belnitskii/birthdayreminderbot/model/Person.java
@@ -1,6 +1,9 @@
 package com.belnitskii.birthdayreminderbot.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,7 +20,9 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 100)
     private String name;
+    @Column(nullable = false)
     private LocalDate birthdayDate;
 
     public int getAge() {
